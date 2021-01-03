7b661cef6df1755ee89217f1100d3c3c3f6a5789
Jude K <judekuti@gmail.com>
2021-01-03 02:24:29 -0600

    Include new welcome message

--- src/components/containers/ColumnLayout.js
+++ src/components/containers/ColumnLayout.js
@@ -8,6 +8,10 @@ const ColumnLayout = ({data}) => {
                 <div className="col-md-12 ">
                     <h2 className="text-uppercase text-center pb-2">{data.title}</h2>
                     <p className="text-justify">{data.content}</p>
+                    <p className="text-justify">{data?.content2}</p>
+                    <p className="text-justify">{data?.content3}</p>
+                    <p>{data?.signature}</p>
+
                 </div>
             </div>
         </>
