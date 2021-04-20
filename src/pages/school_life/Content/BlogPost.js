7575514ec36ad9e99b212108ddbdb1a191e70956
Jude K <judekuti@gmail.com>
2021-04-19 23:45:18 -0500

    Include new posters and daily routine

--- src/pages/school_life/Content/BlogPost.js
+++ src/pages/school_life/Content/BlogPost.js
@@ -1,11 +1,13 @@
 import React, { Fragment } from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 const BlogPost = () => {
   return (
     <Fragment>
       <div className="blog-details-top">
-        <p>The four houses are named after the following Saints of the church:</p>
+        <p>
+          The four houses are named after the following Saints of the church:
+        </p>
         <div className="blog-details-img">
           <img
             alt=""
@@ -13,30 +15,56 @@ const BlogPost = () => {
           />
         </div>
         <div className="blog-details-content text-justify">
-
-
           <h3> ST AGNES</h3>
           <p className="mb-2">
-            St. Agnes was a Roman girl who was only thirteen years old when she suffered martyrdom for her Faith. Agnes had made a promise to God never to stain her purity. Her love for the Lord was very great and she hated sin even more than death! Since she was very beautiful, many young men wished to marry Agnes, but she would always say, "Jesus Christ is my only Spouse."  Procop, the Governor's son, became very angry when she refused him.  Procop accused her of being a Christian and brought her to his father, the Governor.
+            St. Agnes was a Roman girl who was only thirteen years old when she
+            suffered martyrdom for her Faith. Agnes had made a promise to God
+            never to stain her purity. Her love for the Lord was very great and
+            she hated sin even more than death! Since she was very beautiful,
+            many young men wished to marry Agnes, but she would always say,
+            "Jesus Christ is my only Spouse." Procop, the Governor's son, became
+            very angry when she refused him. Procop accused her of being a
+            Christian and brought her to his father, the Governor.
           </p>
-          <p className="mb-2">The Governor promised Agnes wonderful gifts if she would only deny God, but Agnes refused. He tried to change her mind by putting her in chains, but her lovely face shone with joy. Next he sent her to a place of sin, but an Angel protected her. At last, she was condemned to death. Even the pagans cried to see such a young and beautiful girl going to death. Yet, Agnes was as happy as a bride on her wedding day. She did not pay attention to those who begged her to save herself. "I would offend my Spouse," she said, "if I were to try to please you. He chose me first and He shall have me!" Then she prayed and bowed her head for the death-stroke of the sword. St Agnes is celebrated in the church every 21st of January. </p>
+          <p className="mb-2">
+            The Governor promised Agnes wonderful gifts if she would only deny
+            God, but Agnes refused. He tried to change her mind by putting her
+            in chains, but her lovely face shone with joy. Next he sent her to a
+            place of sin, but an Angel protected her. At last, she was condemned
+            to death. Even the pagans cried to see such a young and beautiful
+            girl going to death. Yet, Agnes was as happy as a bride on her
+            wedding day. She did not pay attention to those who begged her to
+            save herself. "I would offend my Spouse," she said, "if I were to
+            try to please you. He chose me first and He shall have me!" Then she
+            prayed and bowed her head for the death-stroke of the sword. St
+            Agnes is celebrated in the church every 21st of January.{" "}
+          </p>
         </div>
       </div>
       <hr />
       <div className="blog-details-top">
-
         <div className="blog-details-img">
           <img
             alt=""
             src={process.env.PUBLIC_URL + "/assets/img/saints/stcatherine.jpg"}
           />
-
         </div>
 
         <div className="blog-details-content text-justify">
           <h3>ST CATHERINE OF SIENA: DOCTOR OF THE CHURCH</h3>
           <p className="mb-2">
-            Born in 1347.  The 25th child of a wool dyer in northern Italy, St. Catherine started having mystical experiences when she was only 6, seeing guardian angels as clearly as the people they protected. She became a Dominican tertiary when she was 16, and continued to have visions of Christ, Mary, and the saints.  St. Catherine was one of the most brilliant theological minds of her day, although she never had any formal education. She persuaded the Pope to go back to Rome from Avignon, in 1377, and when she died, she was endeavoring to heal the Great Western Schism. St, Catherine's letters are considered among the most brilliant writings in the history of the Catholic Church. She died when she was only 33 in 1380 and her body was found incorrupt in 1430. Feast day of St. Catherine is April 29.
+            Born in 1347. The 25th child of a wool dyer in northern Italy, St.
+            Catherine started having mystical experiences when she was only 6,
+            seeing guardian angels as clearly as the people they protected. She
+            became a Dominican tertiary when she was 16, and continued to have
+            visions of Christ, Mary, and the saints. St. Catherine was one of
+            the most brilliant theological minds of her day, although she never
+            had any formal education. She persuaded the Pope to go back to Rome
+            from Avignon, in 1377, and when she died, she was endeavoring to
+            heal the Great Western Schism. St, Catherine's letters are
+            considered among the most brilliant writings in the history of the
+            Catholic Church. She died when she was only 33 in 1380 and her body
+            was found incorrupt in 1430. Feast day of St. Catherine is April 29.
           </p>
         </div>
       </div>
@@ -45,18 +73,41 @@ const BlogPost = () => {
         <div className="blog-details-img">
           <img
             alt=""
-            src={process.env.PUBLIC_URL + "/assets/img/saints/stmariagoretti.png"}
+            src={
+              process.env.PUBLIC_URL + "/assets/img/saints/stmariagoretti.png"
+            }
           />
-
         </div>
-
 
         <div className="blog-details-content text-justify">
           <h3>ST MARIA GORRETTI</h3>
           <p className="mb-2">
-            Born in Corinaldo, Ancona, Italy, on October 16, 1890; her farm worker father moved his family to Ferrier di Conca, near Anzio. Her father died of malaria and her mother had to struggle to feed her children.   In 1902 an eighteen-year-old neighbour, Alexander, grabbed her from her steps and tried to rape her. When Maria said that she would rather die than submit, Alexander began stabbing her with a knife.  As she lay in the hospital, she forgave Alexander before she died. Her death didn't end her forgiveness, however.  Alexander was captured and sentenced to thirty years. He was unrepentant until he had a dream that he was in a garden. Maria was there and gave him flowers. When he woke, he was a changed man, repenting of his crime and living a reformed life. When he was released after 27 years, he went directly to Maria's mother to beg her forgiveness, which she gave.
+            Born in Corinaldo, Ancona, Italy, on October 16, 1890; her farm
+            worker father moved his family to Ferrier di Conca, near Anzio. Her
+            father died of malaria and her mother had to struggle to feed her
+            children. In 1902 an eighteen-year-old neighbour, Alexander, grabbed
+            her from her steps and tried to rape her. When Maria said that she
+            would rather die than submit, Alexander began stabbing her with a
+            knife. As she lay in the hospital, she forgave Alexander before she
+            died. Her death didn't end her forgiveness, however. Alexander was
+            captured and sentenced to thirty years. He was unrepentant until he
+            had a dream that he was in a garden. Maria was there and gave him
+            flowers. When he woke, he was a changed man, repenting of his crime
+            and living a reformed life. When he was released after 27 years, he
+            went directly to Maria's mother to beg her forgiveness, which she
+            gave.
           </p>
-          <p className="mb-2"> When Maria was declared a saint in 1950, Alexander was there in the St. Peter's crowd to celebrate her canonization. She was canonized by Pope Pius XII in 1950 for her purity as model for youth. She is called a martyr because she fought against Alexander's attempts at sexual assault. However, the most important aspect of her story is her forgiveness of her attacker -- her concern for her enemy extending even beyond death. Her feast day is July 6.  St. Maria Goretti is the patroness of youth and for the victims of rape.</p>
+          <p className="mb-2">
+            {" "}
+            When Maria was declared a saint in 1950, Alexander was there in the
+            St. Peter's crowd to celebrate her canonization. She was canonized
+            by Pope Pius XII in 1950 for her purity as model for youth. She is
+            called a martyr because she fought against Alexander's attempts at
+            sexual assault. However, the most important aspect of her story is
+            her forgiveness of her attacker -- her concern for her enemy
+            extending even beyond death. Her feast day is July 6. St. Maria
+            Goretti is the patroness of youth and for the victims of rape.
+          </p>
         </div>
       </div>
       <hr />
@@ -66,19 +117,47 @@ const BlogPost = () => {
             alt=""
             src={process.env.PUBLIC_URL + "/assets/img/saints/sttherese.jpg"}
           />
-
         </div>
 
         <div className="blog-details-content text-justify">
           <h3>ST THERESE OF LISIEUX</h3>
           <p className="mb-2">
-            Born in France in 1873. Tragedy and loss came quickly to Therese when her mother died of breast cancer when she was four and a half years old. Her sixteen-year-old sister Pauline became her second mother which made the second loss even worse when Pauline entered the Carmelite convent five years later.  A few months later, Therese became so ill with a fever that people thought she was dying but she was miraculously cured.  Without realizing it, by the time she was eleven years old she had developed the habit of mental prayer. She would find a place between her bed and the wall and in that solitude think about God, life, eternity. Generations of Catholics have admired this young saint, called her the "Little Flower".  They have found in her short life more inspiration for their lives than in volumes by theologians.  She often said to herself “The only way I can prove my love is by scattering flowers and these flowers are every little sacrifice, every glance and word, and the doing of the least actions for love." She took every chance to sacrifice, no matter how small it would seem. Yet Therese died when she was 24, after having lived as cloistered Carmelite for less than ten years. She never went on missions, never founded a religious order, and never performed great works.
+            Born in France in 1873. Tragedy and loss came quickly to Therese
+            when her mother died of breast cancer when she was four and a half
+            years old. Her sixteen-year-old sister Pauline became her second
+            mother which made the second loss even worse when Pauline entered
+            the Carmelite convent five years later. A few months later, Therese
+            became so ill with a fever that people thought she was dying but she
+            was miraculously cured. Without realizing it, by the time she was
+            eleven years old she had developed the habit of mental prayer. She
+            would find a place between her bed and the wall and in that solitude
+            think about God, life, eternity. Generations of Catholics have
+            admired this young saint, called her the "Little Flower". They have
+            found in her short life more inspiration for their lives than in
+            volumes by theologians. She often said to herself “The only way I
+            can prove my love is by scattering flowers and these flowers are
+            every little sacrifice, every glance and word, and the doing of the
+            least actions for love." She took every chance to sacrifice, no
+            matter how small it would seem. Yet Therese died when she was 24,
+            after having lived as cloistered Carmelite for less than ten years.
+            She never went on missions, never founded a religious order, and
+            never performed great works.
           </p>
-          <p className="mb-2"> The only book of hers, published after her death, was a brief edited version of her journal called "Story of a Soul."  But within 28 years of her death, the public demand was so great that she was canonized. Therese of Lisieux is one of the patron saints of the missions, not because she ever went anywhere, but because of her special love of the missions, and the prayers and letters she gave in support of missionaries. This is reminder to all of us who feel we can do nothing, that it is the little things that keep God's kingdom growing.   Her Feast day is 1st of October but in Nigeria this has been shifted to the 2nd of October every year. </p>
+          <p className="mb-2">
+            {" "}
+            The only book of hers, published after her death, was a brief edited
+            version of her journal called "Story of a Soul." But within 28 years
+            of her death, the public demand was so great that she was canonized.
+            Therese of Lisieux is one of the patron saints of the missions, not
+            because she ever went anywhere, but because of her special love of
+            the missions, and the prayers and letters she gave in support of
+            missionaries. This is reminder to all of us who feel we can do
+            nothing, that it is the little things that keep God's kingdom
+            growing. Her Feast day is 1st of October but in Nigeria this has
+            been shifted to the 2nd of October every year.{" "}
+          </p>
         </div>
       </div>
-
-
     </Fragment>
   );
 };
--- src/pages/school_life/boarding_house.js
+++ src/pages/school_life/boarding_house.js
@@ -5,7 +5,6 @@ import { BreadcrumbsItem } from "react-breadcrumbs-dynamic";
 import LayoutOne from "layouts/LayoutOne";
 import Breadcrumb from "wrappers/breadcrumb/Breadcrumb";
 import BlogSidebar from "./Content/BlogSidebar";
-// import BlogComment from "../../wrappers/blog/BlogComment";
 import BlogPost from "./Content/BlogPost";
 
 const Boarding_House = ({ location }) => {
@@ -15,10 +14,7 @@ const Boarding_House = ({ location }) => {
     <Fragment>
       <MetaTags>
         <title>SMGC | Boarding House</title>
-        <meta
-          name="description"
-          content="Boarding House Page of SMGC"
-        />
+        <meta name="description" content="Boarding House Page of SMGC" />
       </MetaTags>
       <BreadcrumbsItem to={process.env.PUBLIC_URL + "/"}>Home</BreadcrumbsItem>
       <BreadcrumbsItem to={process.env.PUBLIC_URL + pathname}>
@@ -29,6 +25,289 @@ const Boarding_House = ({ location }) => {
         <Breadcrumb />
         <div className="blog-area pt-100 pb-100">
           <div className="container">
+            <h3>Daily Routine</h3>
+            <div className="row">
+              <div className="col-md-4">
+                <table class="table table-bordered">
+                  <thead>
+                    <tr>
+                      <th>MONDAY- FRIDAY</th>
+                      <th>ACTIVITIES</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>4.50a.m.</td>
+                      <td>Rising & Morning Prayer</td>
+                    </tr>
+                    <tr>
+                      <td>5.00a.m. -5. 35a.m</td>
+                      <td>Personal Toileting</td>
+                    </tr>
+                    <tr>
+                      <td>5.40a.m. -6.10a.m.</td>
+                      <td>Morning Services</td>
+                    </tr>
+                    <tr>
+                      <td>6.10a.m. -7.10a.m.</td>
+                      <td>Morning Prep</td>
+                    </tr>
+                    <tr>
+                      <td>7.15a.m. -7.35a.m.</td>
+                      <td>Breakfast</td>
+                    </tr>
+                    <tr>
+                      <td>7.40a.m. -8.05a.m.</td>
+                      <td>Morning Assembly</td>
+                    </tr>
+                    <tr>
+                      <td>8.10a.m. -2.20p.m.</td>
+                      <td>School Hours</td>
+                    </tr>
+                    <tr>
+                      <td>2.25p.m. -2.55p.m.</td>
+                      <td>Lunch</td>
+                    </tr>
+                    <tr>
+                      <td>3.00p.m. -4.00p.m.</td>
+                      <td>
+                        Monday, Wednesday & Thursday- Siesta <br />
+                        Tuesday -Laundry (Washing) <br />
+                        Wednesday - Laundry (Ironing) <br />
+                        Friday- Laundry (Washing)
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>4.00p.m. -4.30p.m.</td>
+                      <td>Freshening Up</td>
+                    </tr>
+                    <tr>
+                      <td>4.35p.m. -5.45p.m.</td>
+                      <td>
+                        Monday - Rosary @ 5p.m. & Mass @ 5.30p.m. <br />
+                        Tuesday - Games <br />
+                        Wednesday - Rosary @ 5p.m. & Mass @ 5.30p.m. <br />
+                        Thursday - Games <br />
+                        Friday - Mass @5.30p.m.
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>5.50p.m. -6.15p.m.</td>
+                      <td>Supper</td>
+                    </tr>
+                    <tr>
+                      <td>6.20p.m. -6.50p.m.</td>
+                      <td>Evening Prayers (Tuesdays & Thursdays)</td>
+                    </tr>
+                    <tr>
+                      <td>6.50p.m.-9.00p.m.</td>
+                      <td>Night Prep</td>
+                    </tr>
+                    <tr>
+                      <td>9.00p.m.- 9.15p.m.</td>
+                      <td>Post Supper</td>
+                    </tr>
+                    <tr>
+                      <td>9.15p.m.-10.00p.m.</td>
+                      <td>Personal Toileting</td>
+                    </tr>
+                    <tr>
+                      <td>10.00p.m.- 10.15p.m.</td>
+                      <td>Night Prayer/Meditation</td>
+                    </tr>
+                    <tr>
+                      <td>10.15p.m.</td>
+                      <td>Lights Out</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+              <div className="col-md-4">
+                <table class="table table-bordered">
+                  <thead>
+                    <tr>
+                      <th>SATURDAY</th>
+                      <th>ACTIVITIES</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>5.45a.m.</td>
+                      <td>Rising</td>
+                    </tr>
+                    <tr>
+                      <td>5.45a.m. – 6.30a.m.</td>
+                      <td>Personal Toileting</td>
+                    </tr>
+                    <tr>
+                      <td>6.30a.m. – 7.00a.m.</td>
+                      <td>Morning Prayers</td>
+                    </tr>
+                    <tr>
+                      <td>7.00a.m.-8.50a.m.</td>
+                      <td>General Cleaning</td>
+                    </tr>
+                    <tr>
+                      <td>9.00a.m.-9.40a.m.</td>
+                      <td>Breakfast</td>
+                    </tr>
+                    <tr>
+                      <td>10.00a.m.-12.00p.m.</td>
+                      <td>Weekend Classes/Morning Prep</td>
+                    </tr>
+                    <tr>
+                      <td>12.15p.m.-1.30p.m.</td>
+                      <td>House Meeting</td>
+                    </tr>
+                    <tr>
+                      <td>1.45p.m.-2.15p.m.</td>
+                      <td>Lunch</td>
+                    </tr>
+                    <tr>
+                      <td>2.20p.m.-3.20p.m.</td>
+                      <td>Siesta/laundry (ironing)</td>
+                    </tr>
+                    <tr>
+                      <td>3.30p.m.-4.40p.m.</td>
+                      <td>Freshening Up</td>
+                    </tr>
+                    <tr>
+                      <td>4.35p.m. -5.45p.m.</td>
+                      <td>Visit to the box room/Laundry (ironing)</td>
+                    </tr>
+                    <tr>
+                      <td>4.45p.m.-5.45p.m.</td>
+                      <td>Choir Practice/Liturgy Preparation</td>
+                    </tr>
+                    <tr>
+                      <td>6.00p.m.-6.30p.m.</td>
+                      <td>Supper</td>
+                    </tr>
+                    <tr>
+                      <td>6.30p.m.-7-00p.m.</td>
+                      <td>Evening Prayers</td>
+                    </tr>
+                    <tr>
+                      <td>7.00p.m.-8.30p.m.</td>
+                      <td>Socials/Prep</td>
+                    </tr>
+                    <tr>
+                      <td>8.30p.m.-9.00p.m.</td>
+                      <td>Post Supper</td>
+                    </tr>
+                    <tr>
+                      <td>9.00p.m.-9.45p.m.</td>
+                      <td>Personal Toileting</td>
+                    </tr>
+                    <tr>
+                      <td>9.45p.m.-10.00p.m.</td>
+                      <td>Meditation/Night Prayers</td>
+                    </tr>
+                    <tr>
+                      <td>10.00p.m.</td>
+                      <td>Lights Out</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+              <div className="col-md-4">
+                {" "}
+                <table class="table table-bordered">
+                  <thead>
+                    <tr>
+                      <th>SUNDAY</th>
+                      <th>ACTIVITIES</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>5.30a.m.</td>
+                      <td>Rising</td>
+                    </tr>
+                    <tr>
+                      <td>5.30a.m. – 6.20a.m.</td>
+                      <td>Personal Toileting</td>
+                    </tr>
+                    <tr>
+                      <td>6.30a.m. – 6.55a.m.</td>
+                      <td>Seated for Mass/Rosary</td>
+                    </tr>
+                    <tr>
+                      <td>7.00a.m.-8.30a.m.</td>
+                      <td>Mass</td>
+                    </tr>
+                    <tr>
+                      <td>8.40a.m.-9.20a.m.</td>
+                      <td>Morning Service</td>
+                    </tr>
+                    <tr>
+                      <td>9.20a.m.-9.50a.m.</td>
+                      <td>Breakfast</td>
+                    </tr>
+                    <tr>
+                      <td>10.00a.m.-11.50a.m.</td>
+                      <td>Morning Prep</td>
+                    </tr>
+                    <tr>
+                      <td>12.00p.m.-1.00p.m.</td>
+                      <td>Prayer Meeting/Moral Instructions</td>
+                    </tr>
+                    <tr>
+                      <td>1.10p.m.-1.50p.m.</td>
+                      <td>Pious Societies</td>
+                    </tr>
+                    <tr>
+                      <td>2.00p.m.-2.30p.m.</td>
+                      <td>Lunch</td>
+                    </tr>
+                    <tr>
+                      <td>2.40p.m.-3.45p.m.</td>
+                      <td>Siesta</td>
+                    </tr>
+                    <tr>
+                      <td>4.00p.m.-5.00p.m.</td>
+                      <td>Catechism Classes/Free Time</td>
+                    </tr>
+                    <tr>
+                      <td>5.00p.m.-6.00p.m.</td>
+                      <td>Games</td>
+                    </tr>
+                    <tr>
+                      <td>6.00p.m.-6.30p.m.</td>
+                      <td>Supper</td>
+                    </tr>
+                    <tr>
+                      <td>6.30p.m.-7-00p.m.</td>
+                      <td>Evening Prayers</td>
+                    </tr>
+                    <tr>
+                      <td>7.00p.m.-9.00p.m.</td>
+                      <td>Night Prep</td>
+                    </tr>
+                    <tr>
+                      <td>9:00p.m. - 9:15p.m.</td>
+                      <td>Post Supper</td>
+                    </tr>
+                    <tr>
+                      <td>9:15p.m. - 10:00p.m.</td>
+                      <td>Personal Toileting</td>
+                    </tr>
+                    <tr>
+                      <td>10:00p.m.-10.15p.m.</td>
+                      <td>Meditation/Night Prayers</td>
+                    </tr>
+                    <tr>
+                      <td>10.15p.m.</td>
+                      <td>Lights Out</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+          <hr />
+          <div className="container">
+            <h3>Boarding House Structure</h3>
             <div className="row flex-md-row-reverse">
               <div className="col-lg-9">
                 <div className="blog-details-wrapper">
@@ -52,7 +331,7 @@ const Boarding_House = ({ location }) => {
 };
 
 Boarding_House.propTypes = {
-  location: PropTypes.object
+  location: PropTypes.object,
 };
 
 export default Boarding_House;
